d54c214160a86b08a9b2172e21bf7a5b29c2269d
Vladimir Romanov <valgard5@mail.ru>
2022-09-27 19:43:57 +0300

    initial commit

---
+++ Homework3.5.1/Homework3.5.1.cpp
@@ -0,0 +1,63 @@
+﻿// Homework3.5.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+class Figure
+{
+protected:
+    int sides_count = 0;
+    std::string name = "Фигура";
+public:
+    int get_sides_count()
+    {
+        return sides_count;
+    }
+    std::string get_name()
+    {
+        return name;
+    }
+};
+class Triangle : public Figure
+{
+public:
+    Triangle() : Figure()
+    {
+        name = "Треугольник";
+        sides_count = 3;
+    }
+};
+class Quadrangle : public Figure
+{
+public:
+    Quadrangle() : Figure()
+    {
+        name = "Четырёхугольник";
+        sides_count = 4;
+    }
+};
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "Russian");
+
+    Figure figure;
+    Triangle triangle;
+    Quadrangle quadrangle;
+
+    std::cout << "Количество сторон: " << std::endl;
+    std::cout << figure.get_name() << ": " << figure.get_sides_count() << std::endl;
+    std::cout << triangle.get_name() << ": " << triangle.get_sides_count() << std::endl;
+    std::cout << quadrangle.get_name() << ": " << quadrangle.get_sides_count() << std::endl;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
